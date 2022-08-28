beef0b82fe986e9435c3c0770faa4279624b0649
hanashiro <felipeaah@gmail.com>
2022-08-28 16:21:44 -0300

    Updates

---
+++ precache-manifest.973d94948333c63e825261ec7562aae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ded41510878d2891f5bd41515fd668",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3aa3511b3b58fec6f826",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "495fc4d588798a3bd48e",
+    "url": "./static/js/2.c7f8e814.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c7f8e814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aa3511b3b58fec6f826",
+    "url": "./static/js/main.3e9fb6f5.chunk.js"
+  },
+  {
+    "revision": "2b505f56968f68166a74",
+    "url": "./static/js/runtime-main.3babaa94.js"
+  }
+]);
