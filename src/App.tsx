8a55950e3ae183d85eb62be1526fa225e5b90963
hanashiro <felipeaah@gmail.com>
2022-08-28 16:17:29 -0300

    test

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'my-test-library'
+import 'my-test-library/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text='Create React Library Example 😄' />
+}
+
+export default App
